be57bbaf76ddfc187d6bb0f9b4b74ad3bc04db48
ChuViaD <131736749+ChuViaD@users.noreply.github.com>
2024-05-16 22:56:54 +0700

    Xem đơn hàng

--- homepage_pages/frmThongTinDonHang.php
+++ homepage_pages/frmThongTinDonHang.php
@@ -1,4 +1,9 @@
 <!-- Full screen modal -->
+<?php 
+    require_once("database.php");
+    $user = $_SESSION["user"];
+    echo $user->soDienThoai;
+?>
 <div class="modal" id="thongtindonhang" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-fullscreen">
         <div class="modal-content">
@@ -12,30 +17,38 @@
             <tr>
                 <th scope="col">Mã đơn hàng</th>
                 <th scope="col">Ngày đặt</th>
-                <th scope="col">Tên sách</th>
-                <th scope="col">Hình ảnh</th>
-                <th scope="col">Số lượng</th>
+                <th scope="col">Địa chỉ giao</th>
                 <th scope="col">Thành tiền</th>
+                <th scope="col">Tình trạng</th>
             </tr>
         </thead>
         <tbody>
+            <?php
+            $query = "SELECT * FROM giohang WHERE soDienThoai = $user->soDienThoai AND status = 1;";
+            $result_giohang = $con->query($query);
+            while($giohang = $result_giohang->fetch_assoc()){
+                
+                $query = "SELECT * FROM donhang WHERE idGioHang = ".$giohang['id'].";";
+                $result_donhang = $con->query($query);
+                $donhang = $result_donhang->fetch_assoc();
+            echo'
             <tr>
-                <td>1</td>
-                <td>2021-10-10</td>
-                <td>Harry Potter</td>
-                <td><img src="https://via.placeholder.com/150" alt="Harry Potter"></td>
-                <td>1</td>
-                <td>100000</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>2021-10-10</td>
-                <td>Harry Potter</td>
-                <td><img src="https://via.placeholder.com/150" alt="Harry Potter"></td>
-                <td>1</td>
-                <td>100000</td>
-            </tr>
-
+                <td>',$donhang['id'],'</td>
+                <td>',$donhang['ngayDat'],'</td>
+                <td>',$donhang['diaChiGiao'],'</td>
+                <td>',$giohang['tongTien'],'</td>
+                ';
+                if($donhang['idTinhTrang'] == 1){
+                    echo'
+                    <td>Chưa xử lý</td>
+                    ';}
+                else{
+                    echo'<td>Đã xử lý</td>';
+                }
+                    echo'
+            </tr>';
+            }
+            ?> 
             <!-- Repeat the above <tr> for each order -->
         </tbody>
     </div>
